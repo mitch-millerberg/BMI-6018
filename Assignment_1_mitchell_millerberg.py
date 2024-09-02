863d0c27a6d3293426c29c52bdfff8e88cb241db
mitch-millerberg <mitchell.millerberg@gmail.com>
2024-09-02 12:44:02 -0600

    Update Q7 and Q8

--- Assignment_1_mitchell_millerberg.py
+++ Assignment_1_mitchell_millerberg.py
@@ -3,6 +3,7 @@
 Created on Mon Sep  2 12:14:24 2024
 
 @author: mitch
+
 Question 7
 Jack has been prescribed 5 medications, write a variable named "Medication",
 and store in it the number of medications he has. Print the type of the variable 
@@ -24,6 +25,28 @@ of medications Jack has been prescribed.
 The print(type(Medication)) statement outputs the type of the variable, 
 which in this case will be <class 'int'>, indicating that Medication is an integer.
 
-
+"""
 
 """
+Question 8
+Susan has a weight of 60 kilograms and a height of 1.58 meters. 
+Write Code to Calculate and Print Susan’s Body Mass Index as float. 
+Note: Body Mass Index is a person’s weight in kilograms divided by the square of height 
+in meters.
+Reference: https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.htmlLinks 
+
+Submit your code either here as a text entry, or on your GitHub page in a new 
+repository and past the link here. We encourage you to use GitHub. 
+Don't forget to add comments to your code.
+
+"""
+# This code caclualtes Susans's Body Mass Index
+# Given Susan's weight and height
+weight = 60  # in kilograms
+height = 1.58  # in meters
+
+# Calculate Body Mass Index (BMI)
+bmi = weight / (height ** 2)
+
+# Print the BMI as a float
+print(bmi)
