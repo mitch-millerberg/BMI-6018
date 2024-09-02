23f784ee724bf677dde0ae53afe3fff2cd517539
mitch-millerberg <mitchell.millerberg@gmail.com>
2024-09-02 14:04:49 -0600

    Question_11 update

--- Assignment_1_mitchell_millerberg.py
+++ Assignment_1_mitchell_millerberg.py
@@ -3,7 +3,79 @@
 Created on Mon Sep  2 12:14:24 2024
 
 @author: mitch
+"""
+"""
+Question 1 (Booleans)
+boolean True + boolean True - boolean False = ?
 
+Here's how the expression evaluates step-by-step:
+boolean True is 1
+boolean True is 1
+boolean False is 0
+"""
+Q1_result = True + True - False
+print(Q1_result)
+
+"""
+ Question 2 (Booleans 2)
+
+"""
+logical_tracker = True
+
+# First operation
+logical_tracker = logical_tracker & logical_tracker
+
+# Second operation
+logical_tracker = logical_tracker | (logical_tracker - logical_tracker)
+
+# Final value
+print(logical_tracker)  # This will output: True
+
+"""
+Question 5 (Strings)
+what is the output for the following?
+string = "E. Coli"
+
+string[2]=
+"""
+Q_5 = "E. Coli"
+print(Q_5[2])
+# the result is the " " space between the "." and the "C"
+"""
+Question 6 (Strings 2)
+# Correct the typo
+"""
+string = "I Bove python"
+# Correct the typo
+corrected_string = string[:2] + "L" + string[3:]
+print(corrected_string)
+
+"""
+ Question 9 (Operators 2)
+"""
+# String concatenation
+str1 = "Mitchell "
+str2 = "Millerberg"
+result = str1 + str2
+print("String concatenation:", result)  # Output: my Full name
+
+# Integer addition
+a = 5
+b = 3
+result = a + b
+print("Integer addition:", result)  # Output: 8
+
+"""
+Question 10 (String 3)
+
+"""
+Q10_result = "7" + "3"
+print(Q10_result)  # Output: "73"
+
+Q10_result_int = int("7") + int("3")
+print(Q10_result_int)  # Output: 10
+
+"""
 Question 7
 Jack has been prescribed 5 medications, write a variable named "Medication",
 and store in it the number of medications he has. Print the type of the variable 
