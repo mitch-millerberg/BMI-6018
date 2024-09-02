489877488a0d30597d63f2a622e50ef508acfbbd
mitch-millerberg <mitchell.millerberg@gmail.com>
2024-09-02 12:22:40 -0600

    update Ass_1

---
+++ Assignment_1_mitchell_millerberg.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 12:14:24 2024
+
+@author: mitch
+Question 7
+Jack has been prescribed 5 medications, write a variable named "Medication",
+and store in it the number of medications he has. Print the type of the variable 
+''Medication". Submit your code either here as a text entry, or on your GitHub page
+in a new repository and past the link here. We encourage you to use GitHub. 
+Don't forget to add comments to your code.
+"""
+
+Medication = 5
+
+#print the type of the variable Medication
+print(type(Medication))
+
+"""
+Explanation:
+The variable Medication is assigned the integer value 5, which represents the number 
+of medications Jack has been prescribed.
+
+The print(type(Medication)) statement outputs the type of the variable, 
+which in this case will be <class 'int'>, indicating that Medication is an integer.
+
+
+
+"""
